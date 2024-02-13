5ce3dd4cc9ac081b0dcdc85ac20b2186c41d16a7
Emelie Rosendahl <89969808+Erm0es@users.noreply.github.com>
2024-02-13 18:45:31 +0100

    react training

--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import React from "react"
+import ReactDOM from "react-dom/client"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Header() {
+    return (
+        <header>
+            <nav className="header-nav">
+                <img src="./logo192.png" alt="React logo." className="img" />
+                <ul className="nav-items">
+                    <li>Contact</li>
+                    <li>About</li>
+                    <li>Pricing</li>
+                </ul>
+            </nav>
+        </ header >
+    )
+}
+
+function MainInfo() {
+    return (
+        <div>
+            <h1>Reasons I'm excited to learn React</h1>
+            <ol>
+                <li>It's a popular library, so I'll be
+                    able to fit in with the cool kids!</li>
+                <li>I'm more likely to get a job as a developer
+                    if I know React</li>
+            </ol>
+        </div>
+    )
+}
+
+function Footer() {
+    return (
+        <footer>
+            <small>© 2024 Wiberg development. All rights reserved.</small>
+        </footer>
+    )
+
+}
+
+function Page() {
+    return (
+        <div>
+            <Header />
+            <MainInfo />
+            <Footer />
+        </div>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<Page />)
